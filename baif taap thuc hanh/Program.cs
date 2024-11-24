3f9b85d17c206f49912bab74c475538859d2a54b
anhnguyen13131313 <quocanhnguyen1313@gmail.com>
2024-11-24 14:54:59 +0700

    sdas

---
+++ baif taap thuc hanh/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baif_taap_thuc_hanh
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("khfdshf");
+        }
+    }
+}
